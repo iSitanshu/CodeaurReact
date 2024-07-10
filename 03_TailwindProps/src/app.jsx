b97301c7743a16605392c21df186a57e285043e9
iSitanshu <Sitanshumishra18@gmail.com>
2024-07-10 17:00:53 +0530

    Last of Theory

---
+++ 03_TailwindProps/src/app.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+import Card from './Components/card'
+
+export function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Sitanshu",
+    age: 21
+  }
+
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card username="chaiaurcode" btnText="click me"></Card>
+      <Card username="Sitanshi"></Card>
+    </>
+  )
+}
