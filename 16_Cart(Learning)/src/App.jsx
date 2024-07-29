7b18a355e0a789328281e6b258eadffbc9c6cb5e
iSitanshu <Sitanshumishra18@gmail.com>
2024-07-29 21:52:58 +0530

    Cart(Learning)

---
+++ 16_Cart(Learning)/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Layout from './components/Layout'
+import Home from './Pages/Home'
+import Detail from './Pages/Detail'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/:slug' element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
