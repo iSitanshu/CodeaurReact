d4b9789629f422aad824062665acae76856481bd
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-04 19:45:11 +0530

    Practice

--- 05_currency/src/App.jsx
+++ 05_currency/src/App.jsx
@@ -1,5 +1,26 @@
-import React from 'react'
-import InputBox from './components/InputBox';
+import React, { useState } from 'react'
+import {InputBox} from './components'
+import InfoCurreny from './components/Hooks/InfoCurrency';
+
+
+function App(){
+  const [amount, setAmount] = useState(0)
+  const [from,setFrom] = useState('usd')
+  const [to,setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  const InfoCurrency = InfoCurreny(from)
+
+  const options = Object.keys(InfoCurrency)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () =>
+    {setConvertedAmount(amount * InfoCurrency[to])}
 
 return (
   <div
@@ -13,35 +34,46 @@ return (
         <form
           onSubmit={(e) => {
             e.preventDefault();
-
+            convert()
           }}
         >
           <div className="w-full mb-1">
             <InputBox
               label="From"
-
+              amount={amount}
+              currecyOptions={options}
+              onCurrencyChange={(currency)=>setAmount(amount)}
+              selectCurrency={from}
+              onAmountChange={(amount)=>setAmount(amount)}
             />
           </div>
           <div className="relative w-full h-0.5">
             <button
               type="button"
               className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-
+              onClick={swap}
             >
               swap
             </button>
           </div>
           <div className="w-full mt-1 mb-4">
             <InputBox
-              label="To"
-
+               label="To"
+               amount={convertedAmount}
+               currecyOptions={options}
+               onCurrencyChange={(currency)=>setTo(currency)}
+               selectCurrency={from}
+               amountDisable
             />
           </div>
           <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-            Convert
+            Convert {from.toUpperCase()} to {to.toUpperCase()}
           </button>
         </form>
       </div>
     </div>
   </div>
 );
+}
+
+export default App;
--- 05_currency/src/components/Hooks/InfoCurrency.js
+++ 05_currency/src/components/Hooks/InfoCurrency.js
@@ -1,3 +1,28 @@
-function InfoCurrency = ()=>{
-    
+import { useEffect, useState } from "react";
+
+function InfoCurreny(currency){
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+          .then((res) => {res.json()})
+          .then((res)=>setData(res[currency]))
+    },[currency])
+    return data;
 }
+export default InfoCurreny;
+
+
+// Custom Hooks Notes
+// 1. Customise Hooks can use the default Hooks 
+// 2. function Name(){
+//     return data;
+// } export default Name;
+// 3. parameters which can be used to call the API
+// 4. API Calling
+// 5. useEffect(()=>{
+//         fetch(`url`)
+//         .then((res)=> res.json()) value convert
+//         .then((res)=>setData(res)) value store
+//     },[dependency])
+
+// You know how to design custom Hooks
